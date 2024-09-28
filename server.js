ea0856256631a8ef074aea2dc2a56efe24b73142
rprakashdass <rprakashdass@gmail.com>
2024-09-28 18:40:54 +0530

    update cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://urban-connect-employee.onrender.com/", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
